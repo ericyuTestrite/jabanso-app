210f08452034681da7ba7822cfceec5318cbdf78
Lewis.Chen <borak0310@gmail.com>
2015-10-03 12:08:36 +0800

    初步表單設計 及 表單送出事件取得

--- www/js/joinController.js
+++ www/js/joinController.js
@@ -3,6 +3,23 @@
 
 angular.module('join.controllers', ['starter.services'])
 
-.controller('JoinCtrl', function(){
-	console.log('in SearchCtrl');
-});
+  .controller('JoinCtrl', function ($scope) {
+    console.log('in JoinCtrl');
+    var nowDate = new Date();
+    var monthStr = parseInt(nowDate.getMonth()) + 1;
+    var nowDateStr = nowDate.getFullYear() + '/' + monthStr + '/' + nowDate.getDate();
+
+    //表單初始值
+    $scope.joinform = {};
+    //加入日期為系統日
+    $scope.joinform.joinDate = nowDateStr;
+    //年資預設1年
+    $scope.joinform.seniority = '1';
+
+    //送出表單處理
+    $scope.submit = function () {
+      console.log(JSON.stringify($scope.joinform));
+      //call webservice 寫入DB ...
+    };
+
+  });
