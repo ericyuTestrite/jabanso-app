26785f61b52124b2a82062b35d9c7b5d38be1c87
ericyuTestrite <eric.yu@testritegroup.com>
2015-10-05 09:21:50 +0800

    Added store into localStorage

--- www/js/joinController.js
+++ www/js/joinController.js
@@ -9,17 +9,25 @@ angular.module('join.controllers', ['starter.services'])
     var monthStr = parseInt(nowDate.getMonth()) + 1;
     var nowDateStr = nowDate.getFullYear() + '/' + monthStr + '/' + nowDate.getDate();
 
-    //表單初始值
-    $scope.joinform = {};
-    //加入日期為系統日
-    $scope.joinform.joinDate = nowDateStr;
-    //年資預設1年
-    $scope.joinform.seniority = '1';
+    var profileStr = localStorage.getItem("profile");
+    if(profileStr !== null){
+        var profile = JSON.parse(profileStr);
+        $scope.joinform = profile;
+    }else{
+      //表單初始值
+      $scope.joinform = {};
+      //加入日期為系統日
+      $scope.joinform.joinDate = nowDateStr;
+      //年資預設1年
+      $scope.joinform.seniority = '1';
+    }
 
     //送出表單處理
     $scope.submit = function () {
+      var profileStr = JSON.stringify($scope.joinform);
       console.log(JSON.stringify($scope.joinform));
       //call webservice 寫入DB ...
+      localStorage.setItem("profile", profileStr);
     };
 
   });
