d0c2ebe852d53fcb96d6868c847522e1d27cf4ea
Josephchung2015 <jc.plurker@gmail.com>
2015-10-06 17:26:31 +0800

    Login Homepage revised
    Login Homepage revised by Louise

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,6 +42,12 @@ angular.module('starter.controllers', ['starter.services'])
 })
 
 .controller('WelcomeCtrl', function($scope) {
+   $scope.logins = [ {img: 'img/login1.jpg'},
+                    {img: 'img/login2.jpg'},
+                    {img: 'img/login3.jpg'},
+                    {img: 'img/login4.jpg'}
+                  ];
+    console.log('in WelcomeCtrl');
 })
 
 
